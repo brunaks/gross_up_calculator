6d5a74be1c61ff17b42d2e047079c98c72ac5cbb
bruna <brunak_s@hotmail.com>
2019-07-27 22:27:16 -0300

    Add GUI to show tax results

--- lib/Server.rb
+++ lib/Server.rb
@@ -1,3 +1,17 @@
 require 'sinatra'
+require_relative '../lib/TaxCalculator'
+require_relative '../lib/employee'
+require_relative '../lib/Result'
 
+set :public_folder, File.dirname(__FILE__) + '/static'
 
+get '/' do
+  redirect 'index.html'
+end
+
+post '/calculate' do
+  employee = Employee.new
+  employee.gross = params[:gross].to_f
+  result = TaxCalculator.new.execute(employee)
+  erb :result_page, :locals => {:result => result}
+end
